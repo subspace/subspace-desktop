ce9c4c9cf3c5499ca33d65b67dba1196fe09ff7f
Serge Kovbasiuk <isserge@protonmail.com>
2022-06-25 12:13:29 +0300

    create stateMachine and stateService on boot

--- src/boot/init.ts
+++ src/boot/init.ts
@@ -6,25 +6,42 @@ import { createApi } from '../lib/util';
 import { AutoLauncher } from "../lib/autoLauncher"
 import { appConfig } from "../lib/appConfig";
 
+import { interpret, Interpreter } from 'xstate';
+import { createStateMachine, StateContext } from "../lib/state";
+
 const LOCAL_RPC = process.env.LOCAL_API_WS || "ws://localhost:9947"
 
 declare module "@vue/runtime-core" {
   export interface ComponentCustomProperties {
     $client: Client;
     $autoLauncher: AutoLauncher;
+    $stateService: Interpreter<StateContext>;
   }
 }
 
 export default boot(async ({ app }) => {
-  await appConfig.init()
-  const { nodeName } = (await appConfig.read());
-  globalState.setNodeName(nodeName);
-  await globalState.loadLangData()
-  const api = createApi(LOCAL_RPC);
-  const client = new Client(api);
-  const autoLauncher = new AutoLauncher();
-  await autoLauncher.init();
-  app.config.globalProperties.$client = client;
-  app.config.globalProperties.$autoLauncher = autoLauncher;
-  app.use(VueApexCharts)
+  try {
+    await appConfig.init()
+    const { nodeName } = (await appConfig.read());
+    globalState.setNodeName(nodeName);
+    await globalState.loadLangData()
+    const api = createApi(LOCAL_RPC);
+    const client = new Client(api);
+    const stateMachine = createStateMachine(client);
+    const stateService = interpret(stateMachine);
+    stateService
+      .onTransition((state) => {
+        // TODO: remove in prod
+        console.log('state transition', state)
+      })
+      .start();
+    const autoLauncher = new AutoLauncher();
+    await autoLauncher.init();
+    app.config.globalProperties.$stateService = stateService;
+    app.config.globalProperties.$client = client;
+    app.config.globalProperties.$autoLauncher = autoLauncher;
+    app.use(VueApexCharts)
+  } catch (error) {
+    console.log('boot failed: ', error);
+  }
 })
