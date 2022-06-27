c33a4831117c99c603c176430b94a26d73857fd1
Serge Kovbasiuk <isserge@protonmail.com>
2022-06-27 12:51:26 +0300

    intergrate state machine into Dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -2,7 +2,13 @@
 q-page.q-pl-lg.q-pr-lg.q-pt-md
   .row.justify-center
   .row.q-pb-sm.justify-center
-  div(v-if="!loading")
+  div(v-if="state.matches('connecting')")
+    .flex
+      .absolute-center
+        .row.justify-center
+          q-spinner-orbit(color="grey" size="120px")
+        h4 Connecting to client...
+  div(v-else)
     .row.q-gutter-md.q-pb-md(v-if="!expanded")
       .col
         plotCard(:plot="plot")
@@ -15,31 +21,29 @@ q-page.q-pl-lg.q-pr-lg.q-pt-md
           :farmedTotalEarned="farmedTotalEarned"
           @expand="expand"
         )
-  div(v-else)
-    .flex
-      .absolute-center
-        .row.justify-center
-          q-spinner-orbit(color="grey" size="120px")
-        h4 Connecting to client...
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue"
 import { Notify } from "quasar"
+import { interpret } from "xstate";
+
 import { globalState as global } from "../lib/global"
 import * as util from "../lib/util"
+import { emptyClientData, ClientData, FarmedBlock } from "../lib/types"
+import { appConfig } from "../lib/appConfig"
+import stateMachine from "../lib/state";
 import farmedList from "../components/farmedList.vue"
 import netCard from "../components/netCard.vue"
 import plotCard from "../components/plotCard.vue"
-import { emptyClientData, ClientData, FarmedBlock } from "../lib/types"
-import { appConfig } from "../lib/appConfig"
 const lang = global.data.loc.text.dashboard
 
 export default defineComponent({
   components: { farmedList, netCard, plotCard },
   data() {
-    // TODO remove this.client after invariants are protected
     return {
+      stateService: interpret(stateMachine),
+      state: stateMachine.initialState,
       lang,
       network: {
         state: "starting",
@@ -51,17 +55,16 @@ export default defineComponent({
         message: lang.initializing,
         plotSizeGB: 0
       },
-      global: global.data,
-      globalState: {
-        state: "starting",
-        message: lang.initializing
-      },
+      // global: global.data,
+      // globalState: {
+      //   state: "starting",
+      //   message: lang.initializing
+      // },
       expanded: false,
       util,
-      loading: true,
       unsubscribe: () => null,
       peerInterval: 0,
-      clientData: <ClientData>emptyClientData
+      clientData: <ClientData>emptyClientData,
     }
   },
   computed: {
@@ -72,39 +75,54 @@ export default defineComponent({
       }, 0)
     }
   },
+  created() {
+    this.stateService.onTransition((state) => {
+        // TODO: remove in prod
+        console.log('state transition', state.value)
+        console.log('context', state.context)
+        console.log('state.children.syncMachine.state', state.children.syncMachine);
+        this.state = state;
+      })
+      .start();
+  },
   async mounted() {
     const config = await appConfig.read()
     this.plot.plotSizeGB = config.plot.sizeGB
 
     if (this.$client.isFirstLoad() === false) {
       util.infoLogger("DASHBOARD | starting node")
-      if (config.nodeName !== "") {
-        await this.$client.startNode(config.plot.location, config.nodeName)
-      } else {
-        util.errorLogger("DASHBOARD | node name was empty when tried to start node")
-      }
-      util.infoLogger("DASHBOARD | starting farmer")
-      const farmerStarted = await this.$client.startFarming(config.plot.location, config.plot.sizeGB)
-      if (!farmerStarted) {
-        util.errorLogger("DASHBOARD | Farmer start error!")
-      }
-      util.infoLogger("DASHBOARD | starting block subscription")
+      // TODO: startNode
+      this.stateService.send({ 
+        type: 'START_NODE', 
+        nodeName: config.nodeName, 
+        location: config.plot.location
+      });
+      // if (config.nodeName !== "") {
+      //   await this.$client.startNode(config.plot.location, config.nodeName)
+      // } else {
+      //   util.errorLogger("DASHBOARD | node name was empty when tried to start node")
+      // }
+      // util.infoLogger("DASHBOARD | starting farmer")
+      // const farmerStarted = await this.$client.startFarming(config.plot.location, config.plot.sizeGB)
+      // if (!farmerStarted) {
+      //   util.errorLogger("DASHBOARD | Farmer start error!")
+      // }
+      // util.infoLogger("DASHBOARD | starting block subscription")
       await this.$client.startSubscription();
     }
 
-    this.global.status.state = "loading"
-    this.global.status.message = "loading..."
+    // this.global.status.state = "loading"
+    // this.global.status.message = "loading..."
 
     this.clientData = this.$client.data
-    this.loading = false
 
     this.fetchPeersCount();// fetch initial peers count value
     this.peerInterval = window.setInterval(this.fetchPeersCount, 30000);
 
     this.$client.data.farming.events.on("newBlock", this.newBlock)
     this.$client.data.farming.events.on("farmedBlock", this.farmBlock)
-    this.global.status.state = "live"
-    this.global.status.message = lang.syncedMsg
+    // this.global.status.state = "live"
+    // this.global.status.message = lang.syncedMsg
     await this.checkNodeAndNetwork()
     await this.checkFarmerAndPlot()
   },
